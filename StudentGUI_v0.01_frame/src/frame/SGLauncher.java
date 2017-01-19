c27145ad762498eeb1bace8fb6bff7418fff6935
kosta <kosta@DESKTOP-TRC4IPU>
2017-01-19 17:09:15 +0900

    .

--- StudentGUI_v0.01_frame/src/frame/SGLauncher.java
+++ StudentGUI_v0.01_frame/src/frame/SGLauncher.java
@@ -1,12 +1,9 @@
 package frame;
 
-public class SGLauncher{
+public class SGLauncher {
 
 	public static void main(String[] args) {
 		
-		SGMainFrame frame = new SGMainFrame("학적관리GUI ver0.01");
-
-		frame.initialization();
-		
 	}
+	
 }
--- StudentGUI_v0.01_frame/src/frame/SGMainFrame.java
+++ StudentGUI_v0.01_frame/src/frame/SGMainFrame.java
@@ -1,24 +1,5 @@
 package frame;
 
-import javax.swing.JFrame;
+public class SGMainFrame {
 
-import constants.SGConstants;
-
-public class SGMainFrame extends JFrame{
-
-//	private SGRegisterFrame registerFrame;
-//	private SGSearchFrame searchFrame;
-//	private SGDeleteFrame deleteFrame;
-//	private SGPrintFrame printFrame;
-//	private SGModifyFrame modifyFrame;
-	
-	public SGMainFrame(String title) {
-		super(title);
-	}
-	
-	public void initialization() {
-		this.setSize(SGConstants.MAINFRAME_WIDTH, SGConstants.MAINFRAME_HEIGHT);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-	}
 }
